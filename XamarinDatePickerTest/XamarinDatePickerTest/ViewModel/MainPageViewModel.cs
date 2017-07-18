43275f176fd88634775ecb18b866b91651f4a457
Nico Zhu <v-zhumin@microsoft.com>
2017-07-18 15:09:32 +0800

    Add project files.

---
+++ XamarinDatePickerTest/XamarinDatePickerTest/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDatePickerTest.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private DateTime? _datetime;
+
+        public DateTime? Datetime
+        {
+            get => _datetime;
+
+            set
+            {
+                _datetime = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPageViewModel()
+        {
+            this.Datetime = new DateTime(2011, 12, 18);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
